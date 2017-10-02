a87e7e11af0ec006d75c0e2a78f8a776cc91f37a
William Llanos <Williama.llanos@hotmail.com>
2017-10-02 16:59:07 -0500

    Test Xamarin Forms - XAML & Code

--- XamarinFormsTest/XamarinFormsTest/MainPage.xaml.cs
+++ XamarinFormsTest/XamarinFormsTest/MainPage.xaml.cs
@@ -11,7 +11,41 @@ namespace XamarinFormsTest
 	{
 		public MainPage()
 		{
-			InitializeComponent();
+			//InitializeComponent();
+			StackLayout layout = new StackLayout
+			{
+				Padding = new Thickness(20),
+				Orientation = StackOrientation.Vertical,
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.Fill
+			};
+			
+			Label label = new Label
+			{
+				Text = "Hola!\nPresiona el botón",
+				FontSize = 24,
+				FontAttributes = FontAttributes.Bold,
+				HorizontalTextAlignment = TextAlignment.Center
+			};
+			layout.Children.Add(label);
+
+			Button elButton = new Button
+			{
+				Text = "El Boton",
+				HorizontalOptions = LayoutOptions.Fill
+			};
+			
+			elButton.Clicked += (sender, args) => { DisplayAlert("Hola", "Click", "OK"); };
+			layout.Children.Add(elButton);
+			Content = layout;
+		}
+
+		private async void ElBotonClicked(object sender, EventArgs e)
+		{
+			//DisplayAlert("Clicked", "Haz presionado el botón", "OK");
+			var opt = await DisplayActionSheet("Select an Option", "Cancel", "Destroy", "O1", "New Opt", "NeXT");
+
+			await DisplayAlert("Clicked", opt, "OK");
 		}
 	}
 }
